956d2c13d10a541ad4f283c4cf28ea428fe3610d
Mazvydas0 <mazvydas.svirskas@gmail.com>
2024-12-17 17:45:29 +0200

    build fixes

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,19 @@ dotenv.config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //... other configurations ...
+  // Add serverExternalPackages for WebAssembly support
+  experimental: {
+    serverComponentsExternalPackages: ["@xmtp/user-preferences-bindings-wasm"],
+  },
+
+  // Add Buffer polyfill to the webpack configuration
+  webpack(config) {
+    // Include @xmtp/user-preferences-bindings-wasm in WebAssembly configuration
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    // Return updated config
+    return config;
+  },
 };
 
 export default nextConfig;
